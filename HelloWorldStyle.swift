f76b7e369f24e5207c41901a6bde14e007245727
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-10 01:59:25 +0000

    Added HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Tamer Zreg
+//  Created on 2024-02-08
+//  Version 1.0
+//  Copyright (c) Tamer Zreg. All rights reserved.
+//
+//  Prints Hello World with Style.
+
+// Output to Console
+print("Hello World, with Style!")
